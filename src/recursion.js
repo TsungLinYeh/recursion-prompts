a0068eaf40ed59f6c74f0a33f37f2eadd619bd13
Jeffrey Yeh <nightcrow33@gmail.com>
2020-12-23 15:55:11 +0800

    check palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -135,6 +135,13 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+    var length = string.length;
+    var match = string.slice(0, 1) === string.slice(length - 1, length);
+    if (length === 1 || length === 0) {
+        return true;
+    } else {
+        return match && palindrome(string.slice(1, length - 1));
+    }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
